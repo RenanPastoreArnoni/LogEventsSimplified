4babf0247aab0d7292dd243501accf07db4b1a67
renanarnoni <arnoni.r.p@gmail.com>
2020-03-07 13:01:21 -0300

    Adicionar arquivos de projeto.

---
+++ LogEventsSimplified/LogEvents.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace LogEventsSimplified {
+    public static class LogEvents {
+        const string MyLog = "Mylog";
+        const string MySource = "Mysource";
+
+        public static void logSimpleStringEvent(string LogMessage, EventLogEntryType logEntryType, int EventID) {
+
+            try {
+
+                if (!EventLog.SourceExists(MySource)) {
+
+                    EventLog.CreateEventSource(MySource, MyLog);
+                }
+
+                using (EventLog log = new EventLog()) {
+
+                    log.Source = MySource;
+                    log.Log = MyLog;
+                    log.WriteEntry(LogMessage, logEntryType, EventID);
+                }
+
+            } catch (Exception) {
+
+                throw;
+            }
+        }
+    }
+}
